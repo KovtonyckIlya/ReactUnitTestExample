68b5909d48b12987bfc72f4f90d202ebe082d5ce
KovtonyckIlya <60618136+KovtonyckIlya@users.noreply.github.com>
2022-07-16 13:48:56 +0300

    Update Board.test.js

--- src/tests/Board.test.js
+++ src/tests/Board.test.js
@@ -80,98 +80,98 @@ it('Each row should have 3 columns.', () => {
   });
 });
 
-// it('Board updates with X and O when user clicks on it and renders proper status.', () => {
-//   act(() => {
-//     ReactDOM.render(<BoardWithReducer />, container);
-//   });
-//   const squares = container.querySelectorAll('button');
-//   const status = container.querySelector('#status');
-//   expect(squares[0].textContent).toBe('');
-//   act(() => {
-//     squares[0].dispatchEvent(new MouseEvent('click', {bubbles: true}));
-//   });
-//   expect(status.innerHTML).toBe('Next player: O');
-//   expect(squares[0].textContent).toBe('X');
-//   expect(squares[1].textContent).toBe('');
-//   act(() => {
-//     squares[1].dispatchEvent(new MouseEvent('click', {bubbles: true}));
-//   });
-//   expect(status.innerHTML).toBe('Next player: X');
-//   expect(squares[1].textContent).toBe('O');
-// });
+it('Board updates with X and O when user clicks on it and renders proper status.', () => {
+  act(() => {
+    ReactDOM.render(<BoardWithReducer />, container);
+  });
+  const squares = container.querySelectorAll('button');
+  const status = container.querySelector('#status');
+  expect(squares[0].textContent).toBe('');
+  act(() => {
+    squares[0].dispatchEvent(new MouseEvent('click', {bubbles: true}));
+  });
+  expect(status.innerHTML).toBe('Next player: O');
+  expect(squares[0].textContent).toBe('X');
+  expect(squares[1].textContent).toBe('');
+  act(() => {
+    squares[1].dispatchEvent(new MouseEvent('click', {bubbles: true}));
+  });
+  expect(status.innerHTML).toBe('Next player: X');
+  expect(squares[1].textContent).toBe('O');
+});
 
-// it('Should still only render 5 divs if all squares are selected and the game was a tie.', () => {
-//   act(() => {
-//     ReactDOM.render(<BoardWithReducer />, container);
-//   });
-//   const squares = container.querySelectorAll('button');
-//   const orderOfClicksByIndex = [0,1,2,4,3,5,7,6,8];
-//   orderOfClicksByIndex.forEach(index => {
-//     act(() => {
-//       squares[index].dispatchEvent(new MouseEvent('click', {bubbles: true}));
-//     });
-//   });
-//   const divs = container.querySelectorAll('div');
-//   expect(divs.length).toEqual(5);
-// });
+it('Should still only render 5 divs if all squares are selected and the game was a tie.', () => {
+  act(() => {
+    ReactDOM.render(<BoardWithReducer />, container);
+  });
+  const squares = container.querySelectorAll('button');
+  const orderOfClicksByIndex = [0,1,2,4,3,5,7,6,8];
+  orderOfClicksByIndex.forEach(index => {
+    act(() => {
+      squares[index].dispatchEvent(new MouseEvent('click', {bubbles: true}));
+    });
+  });
+  const divs = container.querySelectorAll('div');
+  expect(divs.length).toEqual(5);
+});
 
-// it('Clicking should only change the column that was clicked, dont change the numbers on chunkIndex Variable.', () => {
-//   act(() => {
-//     ReactDOM.render(<BoardWithReducer />, container);
-//   });
-//   const squares = container.querySelectorAll('.' + squareStyles.square);
-//   act(() => {
-//     squares[2].dispatchEvent(new MouseEvent('click', {bubbles: true}));
-//   });
-//   squares.forEach((_, index) => {
-//     if (index !== 2) {
-//       expect(squares[index].textContent).toBe('');
-//     }
-//   })
-// });
+it('Clicking should only change the column that was clicked, dont change the numbers on chunkIndex Variable.', () => {
+  act(() => {
+    ReactDOM.render(<BoardWithReducer />, container);
+  });
+  const squares = container.querySelectorAll('.' + squareStyles.square);
+  act(() => {
+    squares[2].dispatchEvent(new MouseEvent('click', {bubbles: true}));
+  });
+  squares.forEach((_, index) => {
+    if (index !== 2) {
+      expect(squares[index].textContent).toBe('');
+    }
+  })
+});
 
-// it('Should display winner when someone wins with their symbol', () => {
-//   act(() => {
-//     ReactDOM.render(<BoardWithReducer />, container);
-//   });
-//   const squares = container.querySelectorAll('button');
-//   const orderOfClicksforXtoWin = [0,1,4,2,8];
-//   orderOfClicksforXtoWin.forEach(index => {
-//     act(() => {
-//       squares[index].dispatchEvent(new MouseEvent('click', {bubbles: true}));
-//     });
-//   });
-//   const status = container.querySelector('#status');
-//   expect(status.innerHTML).toBe('Winner: X');
-// });
+it('Should display winner when someone wins with their symbol', () => {
+  act(() => {
+    ReactDOM.render(<BoardWithReducer />, container);
+  });
+  const squares = container.querySelectorAll('button');
+  const orderOfClicksforXtoWin = [0,1,4,2,8];
+  orderOfClicksforXtoWin.forEach(index => {
+    act(() => {
+      squares[index].dispatchEvent(new MouseEvent('click', {bubbles: true}));
+    });
+  });
+  const status = container.querySelector('#status');
+  expect(status.innerHTML).toBe('Winner: X');
+});
 
-// it('If game was a tie status should be Game was a tie.', () => {
-//   act(() => {
-//     ReactDOM.render(<BoardWithReducer />, container);
-//   });
-//   const squares = container.querySelectorAll('button');
-//   const orderOfClicksByIndex = [0,1,2,4,3,5,7,6,8];
-//   const status = container.querySelector('#status');
-//   orderOfClicksByIndex.forEach(index => {
-//     act(() => {
-//       squares[index].dispatchEvent(new MouseEvent('click', {bubbles: true}));
-//     });
-//   });
-//   expect(status.innerHTML).toBe('Game was a tie');
-// });
+it('If game was a tie status should be Game was a tie.', () => {
+  act(() => {
+    ReactDOM.render(<BoardWithReducer />, container);
+  });
+  const squares = container.querySelectorAll('button');
+  const orderOfClicksByIndex = [0,1,2,4,3,5,7,6,8];
+  const status = container.querySelector('#status');
+  orderOfClicksByIndex.forEach(index => {
+    act(() => {
+      squares[index].dispatchEvent(new MouseEvent('click', {bubbles: true}));
+    });
+  });
+  expect(status.innerHTML).toBe('Game was a tie');
+});
 
-// it('When you click reset button, the board resets', () => {
-//   act(() => {
-//     ReactDOM.render(<BoardAndResetWithReducer />, container);
-//   });
-//   const squares = container.querySelectorAll('.' + squareStyles.square);
-//   const resetButton = container.querySelector('#resetButton');
-//   const status = container.querySelector('#status');
-//   act(() => {
-//     resetButton.dispatchEvent(new MouseEvent('click', {bubbles: true}));
-//   });
-//   squares.forEach((_, index) => {
-//     expect(squares[index].textContent).toBe('');
-//   });
-//   expect(status.innerHTML).toBe('Next player: X');
-// });
+it('When you click reset button, the board resets', () => {
+  act(() => {
+    ReactDOM.render(<BoardAndResetWithReducer />, container);
+  });
+  const squares = container.querySelectorAll('.' + squareStyles.square);
+  const resetButton = container.querySelector('#resetButton');
+  const status = container.querySelector('#status');
+  act(() => {
+    resetButton.dispatchEvent(new MouseEvent('click', {bubbles: true}));
+  });
+  squares.forEach((_, index) => {
+    expect(squares[index].textContent).toBe('');
+  });
+  expect(status.innerHTML).toBe('Next player: X');
+});
